6caf7b9956f1935b6cc9043dec73ec6c01ebe206
Shaurya Mittal <mittalshaurya004@gmail.com>
2021-12-22 21:06:13 +0530

    Sphere structure

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -9,11 +9,16 @@ struct Ray{
     vec3 direction;
 };
 
-bool hit_sphere(vec3 center, float radius, Ray r){
-    vec3 oc = r.origin - center;
+struct Sphere{
+    vec3 center;
+    float radius;
+};
+
+bool hit_sphere(Sphere sphere, Ray r){
+    vec3 oc = r.origin - sphere.center;
     float a = dot(r.direction,r.direction);
     float b = 2.0 * dot(oc, r.direction);
-    float c = dot(oc, oc) - radius * radius;
+    float c = dot(oc, oc) - sphere.radius * sphere.radius;
     float discriminant = b * b - 4 * a * c;
     return discriminant > 0;
 }
@@ -23,7 +28,9 @@ void main()
     vec2 st = gl_FragCoord.xy/vec2(x, y);
     Ray r = Ray(camera_origin, normalize(vec3(st.x - 0.5, st.y - 0.5, 1.0)));
 
-    if(hit_sphere(vec3(0,0,0),0.5,r)){
+    Sphere sphere = {vec3(0,0,0),0.5};
+
+    if(hit_sphere(sphere,r)){
         FragColor = vec4(1,0,0,1);
     }
     else{
