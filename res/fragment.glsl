f3133ed6a90183098ceaa186c107bfb373ebd55c
Shaurya Mittal <mittalshaurya004@gmail.com>
2021-12-24 21:53:06 +0530

    Fixed normals , removed build folder submodule

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -14,14 +14,28 @@ struct Sphere{
     float radius;
 };
 
-bool hit_sphere(Sphere sphere, Ray r){
+struct Discrininant {
+    float disc;
+    float inc;
+};
+
+Discrininant hit_sphere(Sphere sphere, Ray r){
     vec3 oc = r.origin - sphere.center;
     float a = dot(r.direction,r.direction);
     float b = 2.0 * dot(oc, r.direction);
     float c = dot(oc, oc) - sphere.radius * sphere.radius;
     float discriminant = b * b - 4 * a * c;
-    return discriminant > 0;
+    Discrininant d;
+
+    d.disc = discriminant;
+    d.inc = (-b -sqrt(discriminant))/(2.0 * a);
+    return d;
 }
+
+vec3 at(Ray ray, float p){
+    return ray.origin + p * ray.direction;
+}
+
 void main()
 {
     vec3 camera_origin = vec3(0,0,2);
@@ -29,11 +43,14 @@ void main()
     Ray r = Ray(camera_origin, normalize(vec3(st.x - 0.5, st.y - 0.5, 1.0)));
 
     Sphere sphere = {vec3(0,0,0),0.5};
-
-    if(hit_sphere(sphere,r)){
-        FragColor = vec4(1,0,0,1);
+    Discrininant d = hit_sphere(sphere, r);
+    
+    if(d.disc > 0){
+        vec3 N = normalize(at(r, d.inc) - sphere.center);
+        FragColor = vec4(N, 1);
     }
     else{
-        FragColor = vec4(st.xy, 1.0,1.0);
+        // FragColor = vec4(st.xy, 1.0, 1.0);
+        FragColor = vec4(1 - st.y+0.7, 1 - st.y+0.7,1 - st.y+0.9, 1.0);
     }
 }
