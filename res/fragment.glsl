599fb7eacfb9c64b3a7bd63c3e45a38b73e47fb6
Shaurya Mittal <mittalshaurya004@gmail.com>
2021-12-25 10:53:39 +0530

    Hit Record : Chapter 6

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -4,6 +4,7 @@ out vec4 FragColor;
 uniform float x;
 uniform float y;
 
+//Data structures
 struct Ray{
     vec3 origin;
     vec3 direction;
@@ -14,26 +15,30 @@ struct Sphere{
     float radius;
 };
 
-struct Discrininant {
-    float disc;
-    float inc;
+struct hit_record{
+    vec3 p;
+    vec3 normal;
+    float t;
 };
 
-Discrininant hit_sphere(Sphere sphere, Ray r){
+//generate vec3 to intersection point for a given distance and ray
+vec3 at(Ray ray, float p){
+    return ray.origin + p * ray.direction;
+}
+
+bool hit_sphere(Sphere sphere, Ray r, out hit_record rec){
     vec3 oc = r.origin - sphere.center;
     float a = dot(r.direction,r.direction);
     float b = 2.0 * dot(oc, r.direction);
     float c = dot(oc, oc) - sphere.radius * sphere.radius;
     float discriminant = b * b - 4 * a * c;
-    Discrininant d;
+    if (discriminant < 0) return false;
 
-    d.disc = discriminant;
-    d.inc = (-b -sqrt(discriminant))/(2.0 * a);
-    return d;
-}
-
-vec3 at(Ray ray, float p){
-    return ray.origin + p * ray.direction;
+    float root = (-b -sqrt(discriminant))/(2.0 * a);
+    rec.t = root;
+    rec.p = at(r, rec.t);
+    rec.normal = (rec.p-sphere.center)/sphere.radius;
+    return true;
 }
 
 void main()
@@ -43,14 +48,12 @@ void main()
     Ray r = Ray(camera_origin, normalize(vec3(st.x - 0.5, st.y - 0.5, 1.0)));
 
     Sphere sphere = {vec3(0,0,0),0.5};
-    Discrininant d = hit_sphere(sphere, r);
+    hit_record rec;
     
-    if(d.disc > 0){
-        vec3 N = normalize(at(r, d.inc) - sphere.center);
-        FragColor = vec4(N, 1);
+    if(hit_sphere(sphere,r,rec)){
+        FragColor = vec4(rec.normal, 1);
     }
     else{
-        // FragColor = vec4(st.xy, 1.0, 1.0);
         FragColor = vec4(1 - st.y+0.7, 1 - st.y+0.7,1 - st.y+0.9, 1.0);
     }
 }
